d4018aff3dd255a3eb73fb401c3eb463aef83e55
Akila Abeykoon <adabeyko+ncstate@ncsu.edu>
2025-12-27 17:58:46 -0500

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',  // This is correct for username.github.io repos
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
